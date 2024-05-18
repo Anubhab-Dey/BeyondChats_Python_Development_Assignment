344bb9bf1a8e450a5ed054b4c49284412837dfe8
Anubhab Dey <anubhabdey2017@gmail.com>
2024-05-18 11:13:28 +0530

    Seperated the methods from the program logic. Added 'Methods.py', and renamed 'Citationer.py' to 'Fetcher.py'

--- Methods.py
+++ Methods.py
@@ -1,9 +1,7 @@
 import json
 import os
-import time
 
 import requests
-from dotenv import load_dotenv
 from tenacity import (
     retry,
     retry_if_exception_type,
@@ -11,18 +9,7 @@ from tenacity import (
     wait_exponential,
 )
 
-# Load environment variables from a .env file
-load_dotenv()
 
-# Fetch the API URL and output directory from environment variables
-API_URL = os.getenv(
-    "API_URL", "https://devapi.beyondchats.com/api/get_message_with_sources"
-)
-OUTPUT_DIR = os.getenv("OUTPUT_DIR", "JSONs")
-PROGRESS_FILE = os.path.join(OUTPUT_DIR, "progress.json")
-
-
-# Define a custom exception for handling HTTP 429 Too Many Requests errors
 class TooManyRequestsError(Exception):
     """Custom exception for handling HTTP 429 Too Many Requests errors."""
 
@@ -159,37 +146,3 @@ def save_progress(progress_file, last_page):
     """
     with open(progress_file, "w") as f:
         json.dump({"last_page": last_page}, f)
-
-
-if __name__ == "__main__":
-    last_processed_page = load_progress(PROGRESS_FILE)
-    page = last_processed_page
-    while True:
-        try:
-            # Fetch data for the current page
-            data = fetch_page(API_URL, page)
-            if not data or "data" not in data or not data["data"]["data"]:
-                # Break the loop if no data is returned or 'data' key is missing
-                break
-            # Process the data to identify citations
-            citations_result = process_data(data)
-            # Save the processed data to a JSON file
-            save_to_json(citations_result, page, OUTPUT_DIR)
-            # Save the progress
-            save_progress(PROGRESS_FILE, page)
-            # Move to the next page
-            page += 1
-            # Sleep to avoid hitting the rate limit too quickly
-            time.sleep(1)
-        except TooManyRequestsError as e:
-            print(f"Error: {e}")
-            # Wait longer before retrying after hitting rate limit
-            time.sleep(60)
-
-    # Delete the progress file if all pages are processed
-    if os.path.exists(PROGRESS_FILE):
-        os.remove(PROGRESS_FILE)
-        print("Progress file deleted.")
-
-    # Print completion message
-    print("All pages processed.")
